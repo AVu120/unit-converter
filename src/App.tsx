e0988d044c76973b1ddd8c54d1d6698327b6be2e
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-08-27 06:41:53 +1000

    Add another row of inputs for minute - second conversion.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {
   default as CentimetersInput,
   default as KilogramsInput,
   default as GramsInput,
+  default as MinutesInput,
+  default as SecondsInput,
 } from "../src/components/input/Input";
 import styles from "./App.module.scss";
 
@@ -17,9 +19,15 @@ interface IMass {
   grams: number | string;
 }
 
+interface ITime {
+  minutes: number | string;
+  seconds: number | string;
+}
+
 function App() {
   const [length, setLength] = useState<ILength>({ meters: 0, centimeters: 0 });
   const [mass, setMass] = useState<IMass>({ kilograms: 0, grams: 0 });
+  const [time, setTime] = useState<ITime>({ minutes: 0, seconds: 0 });
 
   const changeLength = (
     e: ChangeEvent<HTMLInputElement>,
@@ -62,6 +70,27 @@ function App() {
       });
     }
   };
+
+  const changeTime = (
+    e: ChangeEvent<HTMLInputElement>,
+    unit: "min" | "s"
+  ): void => {
+    const stringValue = e.target.value;
+    const numberValue = Number(stringValue);
+    // if a number or "".
+    if (!isNaN(numberValue)) {
+      if (stringValue === "") return setTime({ minutes: "", seconds: "" });
+      if (unit === "min")
+        return setTime({
+          minutes: stringValue.includes(".") ? stringValue : numberValue,
+          seconds: numberValue * 60,
+        });
+      setTime({
+        minutes: numberValue / 60,
+        seconds: stringValue.includes(".") ? stringValue : numberValue,
+      });
+    }
+  };
   return (
     <div className={styles.App}>
       <h1>Unit Converter</h1>
@@ -85,6 +114,16 @@ function App() {
           onChange={(e: ChangeEvent<HTMLInputElement>) => changeMass(e, "g")}
           value={mass.grams}
           label="Gram"
+        />
+        <MinutesInput
+          onChange={(e: ChangeEvent<HTMLInputElement>) => changeTime(e, "min")}
+          value={time.minutes}
+          label="Minute"
+        />
+        <SecondsInput
+          onChange={(e: ChangeEvent<HTMLInputElement>) => changeTime(e, "s")}
+          value={time.seconds}
+          label="Second"
         />
       </div>
     </div>
