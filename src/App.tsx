fedc7131b6265dfa574f42c935117e994f43c1d2
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-08-26 16:26:28 +1000

    Set up base html elements.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <h1>Unit Converter</h1>
+      <div className={styles.grid}>
+        <div className={styles.input}>
+          <input />
+          <label>Meter</label>
+        </div>
+        <div className={styles.input}>
+          <input />
+          Centimeter
+        </div>
+        <div className={styles.input}>
+          <input />
+          <label>Kilogram</label>
+        </div>
+        <div className={styles.input}>
+          <input />
+          <label>Gram</label>
+        </div>
+      </div>
     </div>
   );
 }
