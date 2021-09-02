6a4ba8cd8b6f348bf77c338edfd2f9dff95d3fc2
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-09-03 09:00:29 +1000

    Remove duplicate code from tests.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,185 +1,279 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
 
-describe("App renders correctly", () => {
+describe("Regression Test Suite", () => {
+  type TDomElement = Element | Node | Document | Window;
+  let unitTypeSelector: TDomElement,
+    inputElement1: TDomElement,
+    inputElement2: TDomElement,
+    unitSelector1: TDomElement,
+    unitSelector2: TDomElement;
+
   beforeEach(() => {
     render(<App />);
-  });
-  test("renders title", () => {
-    const titleElement = screen.getByText(/unit converter/i);
-    expect(titleElement).toBeInTheDocument();
-  });
-  test("renders select inputs", () => {
-    const selectElements = screen.getAllByRole("combobox");
-    expect(selectElements.length).toBe(3);
-    expect(selectElements[0]).toHaveDisplayValue("Length");
-    expect(selectElements[1]).toHaveDisplayValue("Meter");
-    expect(selectElements[2]).toHaveDisplayValue("Centimeter");
+    unitTypeSelector = screen.getByTestId("unit-type-selector");
+    inputElement1 = screen.getAllByTestId("unit-input")[0];
+    inputElement2 = screen.getAllByTestId("unit-input")[1];
+    unitSelector1 = screen.getAllByTestId("unit-selector")[0];
+    unitSelector2 = screen.getAllByTestId("unit-selector")[1];
   });
 
-  test("renders text inputs", () => {
-    const inputElements = screen.getAllByTestId("unit-input");
-    expect(inputElements.length).toBe(2);
-    expect(inputElements[0]).toHaveDisplayValue("0");
-    expect(inputElements[1]).toHaveDisplayValue("0");
-  });
-
-  test("renders formula", () => {
-    expect(screen.getByTestId("formula")).toHaveTextContent(
-      /Formula multiply the length value by 100/i
-    );
-  });
-});
-
-describe("All inputs work", () => {
-  beforeEach(() => {
-    render(<App />);
-  });
-  test("Unit type select input works", async () => {
-    const unitTypeSelector = screen.getByTestId("unit-type-selector");
-    fireEvent.change(unitTypeSelector, { target: { value: "Mass" } });
-    await waitFor(() => {
-      expect(unitTypeSelector).toHaveDisplayValue("Mass");
+  describe("App renders correctly", () => {
+    test("renders title", () => {
+      const titleElement = screen.getByText(/unit converter/i);
+      expect(titleElement).toBeInTheDocument();
     });
-  });
-
-  test("unit select input works", async () => {
-    const unitSelectorElements = screen.getAllByTestId("unit-selector");
-    const unitSelector1 = unitSelectorElements[0];
-    fireEvent.change(unitSelector1, { target: { value: "mm" } });
-    await waitFor(() => {
-      expect(unitSelector1).toHaveDisplayValue(["Millimeter"]);
-    });
-  });
-
-  test("units swap correctly", async () => {
-    const unitSelectorElements = screen.getAllByTestId("unit-selector");
-    const [unitSelector1, unitSelector2] = unitSelectorElements;
-    fireEvent.change(unitSelector1, { target: { value: "cm" } });
-    await waitFor(() => {
-      expect(unitSelector1).toHaveDisplayValue(["Centimeter"]);
-      expect(unitSelector2).toHaveDisplayValue(["Meter"]);
-    });
-  });
-
-  test("unit inputs convert and matching formula displays correctly", async () => {
-    const inputElements = screen.getAllByTestId("unit-input");
-    const [inputElement1, inputElement2] = inputElements;
-
-    fireEvent.change(inputElement1, { target: { value: "5" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("5");
-      expect(inputElement2).toHaveValue("500");
+    test("renders unit selectors", () => {
+      expect(unitTypeSelector).toHaveDisplayValue("Length");
+      expect(unitSelector1).toHaveDisplayValue("Meter");
+      expect(unitSelector2).toHaveDisplayValue("Centimeter");
     });
 
-    const unitSelectorElements = screen.getAllByTestId("unit-selector");
-    const [unitSelector1, unitSelector2] = unitSelectorElements;
-
-    fireEvent.change(unitSelector2, { target: { value: "mm" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("0.5");
-      expect(unitSelector1).toHaveDisplayValue(["Meter"]);
-      expect(inputElement2).toHaveValue("500");
-      expect(unitSelector2).toHaveDisplayValue(["Millimeter"]);
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /Formula multiply the length value by 1000/i
-      );
-    });
-
-    fireEvent.change(inputElement1, { target: { value: "100" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("100");
-      expect(inputElement2).toHaveValue("100000");
-    });
-
-    fireEvent.change(unitSelector1, { target: { value: "cm" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("100");
-      expect(unitSelector1).toHaveDisplayValue(["Centimeter"]);
-      expect(inputElement2).toHaveValue("1000");
-      expect(unitSelector2).toHaveDisplayValue(["Millimeter"]);
-
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /Formula multiply the length value by 10/i
-      );
-    });
-    fireEvent.change(unitSelector1, { target: { value: "mm" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("100");
-      expect(unitSelector1).toHaveDisplayValue(["Millimeter"]);
-      expect(inputElement2).toHaveValue("10");
-      expect(unitSelector2).toHaveDisplayValue(["Centimeter"]);
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /Formula divide the length value by 10/i
-      );
-    });
-
-    fireEvent.change(unitSelector1, { target: { value: "μm" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("100");
-      expect(unitSelector1).toHaveDisplayValue(["Micrometer"]);
-      expect(inputElement2).toHaveValue("0.01");
-      expect(unitSelector2).toHaveDisplayValue(["Centimeter"]);
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /Formula divide the length value by 10000/i
-      );
-    });
-
-    fireEvent.change(unitSelector2, { target: { value: "m" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("10000");
-      expect(unitSelector1).toHaveDisplayValue(["Micrometer"]);
-      expect(inputElement2).toHaveValue("0.01");
-      expect(unitSelector2).toHaveDisplayValue(["Meter"]);
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /Formula divide the length value by 1e\+6/i
-      );
-    });
-
-    fireEvent.change(unitSelector2, { target: { value: "cm" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("100");
-      expect(unitSelector1).toHaveDisplayValue(["Micrometer"]);
-      expect(inputElement2).toHaveValue("0.01");
-      expect(unitSelector2).toHaveDisplayValue(["Centimeter"]);
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /Formula divide the length value by 10000/i
-      );
-    });
-
-    fireEvent.change(unitSelector2, { target: { value: "mm" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("10");
-      expect(unitSelector1).toHaveDisplayValue(["Micrometer"]);
-      expect(inputElement2).toHaveValue("0.01");
-      expect(unitSelector2).toHaveDisplayValue(["Millimeter"]);
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /divide the length value by 1000/i
-      );
-    });
-
-    fireEvent.change(unitSelector2, { target: { value: "μm" } });
-    await waitFor(() => {
-      expect(inputElement1).toHaveValue("0.00001");
-      expect(unitSelector1).toHaveDisplayValue(["Millimeter"]);
-      expect(inputElement2).toHaveValue("0.01");
-      expect(unitSelector2).toHaveDisplayValue(["Micrometer"]);
-      expect(screen.getByTestId("formula")).toHaveTextContent(
-        /multiply the length value by 1000/i
-      );
-    });
-
-    fireEvent.change(screen.getByTestId("unit-type-selector"), {
-      target: { value: "Mass" },
-    });
-    await waitFor(() => {
+    test("renders text inputs", () => {
       expect(inputElement1).toHaveValue("0");
-      expect(unitSelector1).toHaveDisplayValue(["Kilogram"]);
       expect(inputElement2).toHaveValue("0");
-      expect(unitSelector2).toHaveDisplayValue(["Gram"]);
+    });
+
+    test("renders formula", () => {
       expect(screen.getByTestId("formula")).toHaveTextContent(
-        /Formula multiply the mass value by 1000/i
+        /Formula multiply the length value by 100/i
       );
+    });
+  });
+
+  describe("All inputs work", () => {
+    test("unit selector works", async () => {
+      fireEvent.change(unitSelector1, { target: { value: "mm" } });
+      await waitFor(() => {
+        expect(unitTypeSelector).toHaveDisplayValue("Length");
+        expect(inputElement1).toHaveValue("0");
+        expect(unitSelector1).toHaveDisplayValue("Millimeter");
+        expect(inputElement2).toHaveValue("0");
+        expect(unitSelector2).toHaveDisplayValue("Centimeter");
+      });
+    });
+    test("Unit type selector works", async () => {
+      fireEvent.change(unitTypeSelector, { target: { value: "Mass" } });
+      await waitFor(() => {
+        expect(unitTypeSelector).toHaveDisplayValue("Mass");
+        expect(inputElement1).toHaveValue("0");
+        expect(unitSelector1).toHaveDisplayValue("Kilogram");
+        expect(inputElement2).toHaveValue("0");
+        expect(unitSelector2).toHaveDisplayValue("Gram");
+      });
+    });
+
+    test("units swap correctly", async () => {
+      fireEvent.change(unitSelector1, { target: { value: "cm" } });
+      await waitFor(() => {
+        expect(unitTypeSelector).toHaveDisplayValue("Length");
+        expect(inputElement1).toHaveValue("0");
+        expect(unitSelector1).toHaveDisplayValue("Centimeter");
+        expect(inputElement2).toHaveValue("0");
+        expect(unitSelector2).toHaveDisplayValue("Meter");
+      });
+    });
+
+    test("unit inputs convert and matching formula displays correctly", async () => {
+      fireEvent.change(inputElement1, { target: { value: "5" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("5");
+        expect(inputElement2).toHaveValue("500");
+      });
+
+      fireEvent.change(unitSelector2, { target: { value: "mm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("0.5");
+        expect(unitSelector1).toHaveDisplayValue("Meter");
+        expect(inputElement2).toHaveValue("500");
+        expect(unitSelector2).toHaveDisplayValue("Millimeter");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula multiply the length value by 1000/i
+        );
+      });
+
+      fireEvent.change(inputElement1, { target: { value: "100" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("100");
+        expect(inputElement2).toHaveValue("100000");
+      });
+
+      fireEvent.change(unitSelector1, { target: { value: "cm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("100");
+        expect(unitSelector1).toHaveDisplayValue("Centimeter");
+        expect(inputElement2).toHaveValue("1000");
+        expect(unitSelector2).toHaveDisplayValue("Millimeter");
+
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula multiply the length value by 10/i
+        );
+      });
+      fireEvent.change(unitSelector1, { target: { value: "mm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("100");
+        expect(unitSelector1).toHaveDisplayValue("Millimeter");
+        expect(inputElement2).toHaveValue("10");
+        expect(unitSelector2).toHaveDisplayValue("Centimeter");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula divide the length value by 10/i
+        );
+      });
+
+      fireEvent.change(unitSelector1, { target: { value: "μm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("100");
+        expect(unitSelector1).toHaveDisplayValue("Micrometer");
+        expect(inputElement2).toHaveValue("0.01");
+        expect(unitSelector2).toHaveDisplayValue("Centimeter");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula divide the length value by 10000/i
+        );
+      });
+
+      fireEvent.change(unitSelector2, { target: { value: "m" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("10000");
+        expect(unitSelector1).toHaveDisplayValue("Micrometer");
+        expect(inputElement2).toHaveValue("0.01");
+        expect(unitSelector2).toHaveDisplayValue("Meter");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula divide the length value by 1e\+6/i
+        );
+      });
+
+      fireEvent.change(unitSelector2, { target: { value: "cm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("100");
+        expect(unitSelector1).toHaveDisplayValue("Micrometer");
+        expect(inputElement2).toHaveValue("0.01");
+        expect(unitSelector2).toHaveDisplayValue("Centimeter");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula divide the length value by 10000/i
+        );
+      });
+
+      fireEvent.change(unitSelector2, { target: { value: "mm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("10");
+        expect(unitSelector1).toHaveDisplayValue("Micrometer");
+        expect(inputElement2).toHaveValue("0.01");
+        expect(unitSelector2).toHaveDisplayValue("Millimeter");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /divide the length value by 1000/i
+        );
+      });
+
+      fireEvent.change(unitSelector2, { target: { value: "μm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("0.00001");
+        expect(unitSelector1).toHaveDisplayValue("Millimeter");
+        expect(inputElement2).toHaveValue("0.01");
+        expect(unitSelector2).toHaveDisplayValue("Micrometer");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /multiply the length value by 1000/i
+        );
+      });
+
+      fireEvent.change(unitTypeSelector, {
+        target: { value: "Mass" },
+      });
+      await waitFor(() => {
+        expect(unitTypeSelector).toHaveDisplayValue("Mass");
+        expect(inputElement1).toHaveValue("0");
+        expect(unitSelector1).toHaveDisplayValue("Kilogram");
+        expect(inputElement2).toHaveValue("0");
+        expect(unitSelector2).toHaveDisplayValue("Gram");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula multiply the mass value by 1000/i
+        );
+      });
+
+      fireEvent.change(inputElement1, { target: { value: "1" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("1");
+        expect(unitSelector1).toHaveDisplayValue("Kilogram");
+        expect(inputElement2).toHaveValue("1000");
+        expect(unitSelector2).toHaveDisplayValue("Gram");
+      });
+
+      fireEvent.change(inputElement2, { target: { value: "523" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("0.523");
+        expect(unitSelector1).toHaveDisplayValue("Kilogram");
+        expect(inputElement2).toHaveValue("523");
+        expect(unitSelector2).toHaveDisplayValue("Gram");
+      });
+
+      fireEvent.change(unitSelector2, { target: { value: "kg" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("523000");
+        expect(unitSelector1).toHaveDisplayValue("Gram");
+        expect(inputElement2).toHaveValue("523");
+        expect(unitSelector2).toHaveDisplayValue("Kilogram");
+        expect(screen.getByTestId("formula")).toHaveTextContent(
+          /Formula divide the mass value by 1000/i
+        );
+      });
+
+      fireEvent.change(inputElement2, { target: { value: "44" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("44000");
+        expect(unitSelector1).toHaveDisplayValue("Gram");
+        expect(inputElement2).toHaveValue("44");
+        expect(unitSelector2).toHaveDisplayValue("Kilogram");
+      });
+
+      fireEvent.change(inputElement1, { target: { value: "3.098" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveValue("3.098");
+        expect(unitSelector1).toHaveDisplayValue("Gram");
+        expect(inputElement2).toHaveValue("0.003098");
+        expect(unitSelector2).toHaveDisplayValue("Kilogram");
+      });
+    });
+  });
+
+  describe("Error handling works correctly", () => {
+    const errorRegex = /Error: only valid numbers allowed./i;
+    test("only show error message when user types anything but valid number.", async () => {
+      fireEvent.change(inputElement1, { target: { value: "a" } });
+      await screen.findByText(errorRegex);
+
+      fireEvent.change(inputElement1, { target: { value: "" } });
+      await waitFor(() => {
+        expect(screen.queryByText(errorRegex)).not.toBeInTheDocument();
+      });
+
+      fireEvent.change(inputElement2, { target: { value: "-" } });
+      await screen.findByText(errorRegex);
+
+      fireEvent.change(inputElement2, { target: { value: "2" } });
+      await waitFor(() => {
+        expect(screen.queryByText(errorRegex)).not.toBeInTheDocument();
+      });
+    });
+
+    test("remove error message and reset values to 0 when user changes unit", async () => {
+      fireEvent.change(inputElement1, { target: { value: "%" } });
+      await screen.findByText(errorRegex);
+
+      fireEvent.change(unitSelector1, { target: { value: "mm" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveDisplayValue("0");
+        expect(inputElement2).toHaveDisplayValue("0");
+        expect(screen.queryByText(errorRegex)).not.toBeInTheDocument();
+      });
+
+      fireEvent.change(inputElement1, { target: { value: "^" } });
+      await screen.findByText(errorRegex);
+
+      fireEvent.change(unitTypeSelector, { target: { value: "Mass" } });
+      await waitFor(() => {
+        expect(inputElement1).toHaveDisplayValue("0");
+        expect(inputElement2).toHaveDisplayValue("0");
+        expect(screen.queryByText(errorRegex)).not.toBeInTheDocument();
+      });
     });
   });
 });
