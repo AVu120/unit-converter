6a572825259704e472c142f68b318f47a8095a00
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-09-04 08:40:50 +1000

    Add time units.

--- src/services/data.ts
+++ src/services/data.ts
@@ -115,6 +115,8 @@ export const data: any = {
       t: "Tonne",
       kg: "Kilogram",
       g: "Gram",
+      mg: "Miligram",
+      μg: "Microgram",
     },
     conversionFunctions: {
       t: {
@@ -126,9 +128,41 @@ export const data: any = {
           (num: number): number => num * 1000000,
           (num: number): number => num / 1000000,
         ],
+        mg: [
+          (num: number): number => num * 1000000000,
+          (num: number): number => num / 1000000000,
+        ],
+        μg: [
+          (num: number): number => num * 1000000000000,
+          (num: number): number => num / 1000000000000,
+        ],
       },
       kg: {
         g: [
+          (num: number): number => num * 1000,
+          (num: number): number => num / 1000,
+        ],
+        mg: [
+          (num: number): number => num * 1000000,
+          (num: number): number => num / 1000000,
+        ],
+        μg: [
+          (num: number): number => num * 1000000000,
+          (num: number): number => num / 1000000000,
+        ],
+      },
+      g: {
+        mg: [
+          (num: number): number => num * 1000,
+          (num: number): number => num / 1000,
+        ],
+        μg: [
+          (num: number): number => num * 1000000,
+          (num: number): number => num / 1000000,
+        ],
+      },
+      mg: {
+        μg: [
           (num: number): number => num * 1000,
           (num: number): number => num / 1000,
         ],
@@ -141,9 +175,126 @@ export const data: any = {
           "divide the mass value by 1000",
         ],
         g: ["multiply the mass value by 1e+6", "divide the mass value by 1e+6"],
+        mg: [
+          "multiply the mass value by 1e+9",
+          "divide the mass value by 1e+9",
+        ],
+        μg: [
+          "multiply the mass value by 1e+12",
+          "divide the mass value by 1e+12",
+        ],
       },
       kg: {
         g: ["multiply the mass value by 1000", "divide the mass value by 1000"],
+        mg: [
+          "multiply the mass value by 1e+6",
+          "divide the mass value by 1e+6",
+        ],
+        μg: [
+          "multiply the mass value by 1e+9",
+          "divide the mass value by 1e+9",
+        ],
+      },
+      g: {
+        mg: [
+          "multiply the mass value by 1000",
+          "divide the mass value by 1000",
+        ],
+        μg: [
+          "multiply the mass value by 1e+6",
+          "divide the mass value by 1e+6",
+        ],
+      },
+      mg: {
+        μg: [
+          "multiply the mass value by 1000",
+          "divide the mass value by 1000",
+        ],
+      },
+    },
+  },
+  Time: {
+    unitsToLabels: {
+      wk: "Week",
+      d: "Day",
+      h: "Hour",
+      m: "Minute",
+      s: "Second",
+    },
+    conversionFunctions: {
+      wk: {
+        d: [(num: number): number => num * 7, (num: number): number => num / 7],
+        h: [
+          (num: number): number => num * 168,
+          (num: number): number => num / 168,
+        ],
+        m: [
+          (num: number): number => num * 10080,
+          (num: number): number => num / 10080,
+        ],
+        s: [
+          (num: number): number => num * 604800,
+          (num: number): number => num / 604800,
+        ],
+      },
+      d: {
+        h: [
+          (num: number): number => num * 24,
+          (num: number): number => num / 24,
+        ],
+        m: [
+          (num: number): number => num * 1440,
+          (num: number): number => num / 1440,
+        ],
+        s: [
+          (num: number): number => num * 86400,
+          (num: number): number => num / 86400,
+        ],
+      },
+      h: {
+        m: [
+          (num: number): number => num * 60,
+          (num: number): number => num / 60,
+        ],
+        s: [
+          (num: number): number => num * 3600,
+          (num: number): number => num / 3600,
+        ],
+      },
+      m: {
+        s: [
+          (num: number): number => num * 60,
+          (num: number): number => num / 60,
+        ],
+      },
+    },
+    formulas: {
+      wk: {
+        d: ["multiply the time value by 7", "divide the time value by 7"],
+        h: ["multiply the time value by 168", "divide the time value by 168"],
+        m: [
+          "multiply the time value by 10080",
+          "divide the time value by 10080",
+        ],
+        s: [
+          "multiply the time value by 604800",
+          "divide the time value by 604800",
+        ],
+      },
+      d: {
+        h: ["multiply the time value by 24", "divide the time value by 24"],
+        m: ["multiply the time value by 1440", "divide the time value by 1440"],
+        s: [
+          "multiply the time value by 86400",
+          "divide the time value by 86400",
+        ],
+      },
+      h: {
+        m: ["multiply the time value by 60", "divide the time value by 60"],
+        s: ["multiply the time value by 3600", "divide the time value by 3600"],
+      },
+      m: {
+        s: ["multiply the time value by 60", "divide the time value by 60"],
       },
     },
   },
